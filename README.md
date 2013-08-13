2dca7cf24f01c66fa98ef4904b4c0b84ea0b8841
Adrian Keenan <adriank09@gmail.com>
2013-08-13 15:41:28 +0100

    file push

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 console-toast-8
 ===============
+An application for displaying widnows 8 notifications from the command line.
 
-Windows 8 toast notifications from CLi
+- Supports:
+    - All templates
+    - Custom shortcut icon (instead of the default which appears on the toast)
+    - Silent toasts
+
+A list of templates can be found [here](http://msdn.microsoft.com/en-us/library/windows/apps/windows.ui.notifications.toasttemplatetype).
+Images must be <200kb and <1024px square.
+Shortcut icons are suggested to be 32x32 for 100% DPI, as larger sizes do not scale down well.
+    
+Switches are listed in Program.cs. The app_id switch value is used to name the shortcut which will appear on the user's Start screen containing an icon.
+
+The only external reference you need is the [Windows API Code Pack](http://archive.msdn.microsoft.com/WindowsAPICodePack). Note you may need to extract the exe manually.
+
+License
+=======
+Public domain, do whatever you want.
---
+++ console-toast-8/console-toast-8/Program.cs
@@ -0,0 +1,71 @@
+﻿using NDesk.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Win8Toast;
+
+namespace console_toast_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string template = "", title = "", line1 = "", line2 = "", img = "", icon = "", app_id = "console-toast-8";
+            bool silent = false;
+
+            OptionSet p = new OptionSet()
+            .Add("template=",   "toast template",                                   x => template = x)
+            .Add("title=",      "toast tile",                                       x => title = x)
+            .Add("line1=",      "line one text (all templates)",                    x => line1 = x)
+            .Add("line2=",      "line two text (Text/Text&Image04 templates)",      x => line2 = x)
+            .Add("img=",        "img url (local or external, all img templates)",   x => img = x)
+            .Add("app_id=",     "name to appear in start menu (optional)",          x => app_id = x)
+            .Add("icon=",       "path to .ico (optional)",                          x => icon = x)
+            .Add("silent",      "prevent default notification noise (optional)",    x => silent = x != null);
+            p.Parse(args);
+
+            if (args.Length == 0 || template == "")
+                Environment.Exit(1);
+
+            Toast.APP_ID = app_id;
+            Toast.silent = silent;
+            Toast.icon_location = icon;
+            Toast.TryCreateShortcut();
+
+            switch (template)
+            {
+                case "ToastImageAndText01":
+                    Toast.toastToastImageAndText01(line1, img);
+                    break;
+                case "ToastImageAndText02":
+                    Toast.toastToastImageAndText02(title, line1, img);
+                    break;
+                case "ToastImageAndText03":
+                    Toast.toastToastImageAndText03(title, line1, img);
+                    break;
+                case "ToastImageAndText04":
+                    Toast.toastToastImageAndText04(title, line1, line2, img);
+                    break;
+                case "ToastText01":
+                    Toast.toastToastText01(line1);
+                    break;
+                case "ToastText02":
+                    Toast.toastToastText02(title, line1);
+                    break;
+                case "ToastText03":
+                    Toast.toastToastText03(title, line1);
+                    break;
+                case "ToastText04":
+                    Toast.toastToastText04(title, line1, line2);
+                    break;
+                default:
+                    Environment.Exit(1);
+                    break;
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
